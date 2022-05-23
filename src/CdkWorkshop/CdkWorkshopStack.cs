660c20ff11e4e17b9812cd855732074ab515c31b
Nigel <nigelsmyth@hotmail.com>
2022-05-23 16:31:10 +0100

    Added files

--- src/CdkWorkshop/CdkWorkshopStack.cs
+++ src/CdkWorkshop/CdkWorkshopStack.cs
@@ -1,8 +1,13 @@
+// This is from the tutorial https://cdkworkshop.com/40-dotnet.html
+
 using Amazon.CDK;
 using Amazon.CDK.AWS.SNS;
+using Amazon.CDK.AWS.Lambda;
+using Cdklabs.DynamoTableViewer;
 using Amazon.CDK.AWS.SNS.Subscriptions;
 using Amazon.CDK.AWS.SQS;
 using Constructs;
+using Amazon.CDK.AWS.APIGateway;
 
 namespace CdkWorkshop
 {
@@ -10,15 +15,32 @@ namespace CdkWorkshop
     {
         internal CdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-             // The CDK includes built-in constructs for most resource types, such as Queues and Topics.
-            var queue = new Queue(this, "CdkWorkshopQueue", new QueueProps
+            // Define a new Lambda resource
+            var hello = new Function(this, "HelloHandler", new FunctionProps
             {
-                VisibilityTimeout = Duration.Seconds(300)
+                Runtime = Runtime.NODEJS_14_X,  // Execution environment
+                Code = Code.FromAsset("lambda"), // code loaded from the lambda directory
+                Handler = "hello.handler" // file is "hello", function is "handler"
             });
 
-            var topic = new Topic(this, "CdkWorkshopTopic");
+            var helloWithCounter = new HitCounter(this, "HelloHitCounter", new HitCounterProps
+            {
+                Downstream = hello
+            });
 
-            topic.AddSubscription(new SqsSubscription(queue));
+            // define an API Gateway REST API resource backed up by our "hello" function
+            new LambdaRestApi(this, "EndPoint", new LambdaRestApiProps
+            {
+                Handler = helloWithCounter.Handler
+            });
+
+            // Defines a new TableViewer resource
+            new TableViewer(this, "ViewerHitCount", new TableViewerProps
+            {
+                Title = "Hello Hits",
+                Table = helloWithCounter.MyTable, 
+                SortBy = "hits"
+            });
         }
     }
 }
